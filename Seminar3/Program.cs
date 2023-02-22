e5e333688b22b54bc5d4ee0ccff7d75db363c5a2
Kosarev Sergei <kosarev90sv@icloud.com>
2023-02-22 04:08:22 +0300

    class work 3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*void ShowDiapozone(int quad){
+
+    if(quad == 1) Console.WriteLine("x > 0 and y > 0");
+    else if(quad == 2) Console.WriteLine("x < 0 and y > 0");
+    else if(quad == 3) Console.WriteLine("x < 0 and y < 0");
+    else if(quad == 4) Console.WriteLine("x > 0 and y < 0");
+    else Console.WriteLine("Uncorrect input!");
+}
+
+    Console.WriteLine("Input a number quardinat: ");
+    int quardinat = Convert.ToInt32(Console.ReadLine());
+
+ShowDiapozone(quardinat);*/
+
+/*Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+и выдаёт номер четверти плоскости, в которой находится эта точка.*/
+
+int NumberQuadrant(int x, int y){
+
+    if(x > 0 && y > 0) return 1;
+    if(x> 0 && y < 0) return 4;
+    if(x> 0 && y < 0) return 2;
+    if(x< 0 && y > 0) return 3;
+
+    return -1;
+}
+
+Console.WriteLine("Input a X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int res = NumberQuadrant(x , y);
+
+if ( res == -1 ) Console.WriteLine("Nit correect ");
+else Console.WriteLine("quardinat " +res);
+
+
+/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.* 4 аргумента */
+
+double Distance(double xa, double xb, double ya, double yb)
+{
+    double firstLenght = xb - xa;
+    double seconLenght = yb - ya;
+}
+
+/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.*/
+
+void SaquareTable(int n){
+
+    int i = 1;
+    while(i <= n){
+
+    }
+}
